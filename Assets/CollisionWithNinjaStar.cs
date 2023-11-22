21ff7678d24bda693477e51273ef1ea6da3d1fae
Douglas Covington <dtc190000@utdallas.edu>
2023-11-22 14:51:13 -0600

    for now I'm sticking with the orginal implementation unless otherwise

--- Assets/CollisionWithNinjaStar.cs
+++ Assets/CollisionWithNinjaStar.cs
@@ -1,82 +1,36 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class CollisionWithNinjaStar : MonoBehaviour
+public class CollisionWithPaddle : MonoBehaviour
 {
-    private List<GameObject> hitList = new List<GameObject>();
+    private bool doesCollideWithPaddle = false;
     private bool stun = false;
-    
+
 
     private void Update()
     {
         bool clicked = Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space);
 
-        if (clicked)
+        if (doesCollideWithPaddle && clicked && !stun)
         {
-            Debug.Log("Count of hit list: " + hitList.Count);
-        }
-
-        if (!(hitList.Count == 0) && clicked && !stun) 
-        {
-            for (int i = 0; i < hitList.Count; i++)
-            {
-                Destroy(hitList[hitList.Count - 1]);
-                //hitList.RemoveAt(hitList.Count - 1);
-                //Debug.Log("Killed projectile");
-            }
-
-            //Debug.Log("Is this empty: " + hitList.ToString());
+            Destroy(this.gameObject);
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Ninja Star")
+        if (collision.gameObject.tag == "Paddle")
         {
-            hitList.Add(collision.gameObject);
+            doesCollideWithPaddle = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Ninja Star")
+        if (collision.gameObject.tag == "Paddle")
         {
-            hitList.Remove(collision.gameObject);
+            doesCollideWithPaddle = false;
         }
     }
-    //private void OnTriggerEnter2D(UnityEngine.Collider2D collision)
-    //{
-    //    if (collision.gameObject.tag.Equals("Paddle"))
-    //    {
-    //        Debug.Log("Hit with paddle");
-    //    }
-    //    else
-    //    {
-    //        Debug.Log("How: " + collision.gameObject.ToString());
-    //    }
-
-    //    if ((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))&& collision.gameObject.tag.Equals("Paddle"))
-    //    {
-    //        Destroy(this.gameObject);
-    //    }
-    //}
-    //private void OnTriggerStay2D(UnityEngine.Collider2D collision)
-    //{
-    //    if (collision.gameObject.tag.Equals("Paddle"))
-    //    {
-    //        Debug.Log("Hit with paddle");
-    //    }
-    //    else
-    //    {
-    //        Debug.Log("How: " + collision.gameObject.ToString());
-    //    }
-
-    //    if ((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) && collision.gameObject.tag.Equals("Paddle"))
-    //    {
-    //        Destroy(this.gameObject);
-    //    }
-    //}
 }
