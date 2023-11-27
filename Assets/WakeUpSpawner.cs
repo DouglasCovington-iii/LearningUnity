4952ad078e35f85a5fa0475d70a52a488132a945
Douglas Covington <dtc190000@utdallas.edu>
2023-11-27 07:40:07 -0600

    I think I'm done

--- Assets/WakeUpSpawner.cs
+++ Assets/WakeUpSpawner.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class WakeUpSpawner : MonoBehaviour
 {
     // Start is called before the first frame update
     public Spawner spawner;
+    public TextMeshPro displayTimer;
+    bool l1 = true;
     void Start()
     {
         
@@ -14,14 +17,25 @@ public class WakeUpSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-            StartCoroutine(WakeUp());
+        if (Input.GetMouseButtonDown(0) && l1)
+        {
+            l1 = false;
+            StartCoroutine(Intro(1));
+        }
     }
 
-    IEnumerator WakeUp()
+    IEnumerator Intro(float waitTime)
     {
-        yield return new WaitForSeconds(2);
+        //Time.fixedDeltaTime = newTimeStep;
+        //Time.maximumDeltaTime = newMaxTimeStep;
+        displayTimer.gameObject.SetActive(true);
+        displayTimer.text = "3";
+        yield return new WaitForSeconds(waitTime);
+        displayTimer.text = "2";
+        yield return new WaitForSeconds(waitTime);
+        displayTimer.text = "1";
+        yield return new WaitForSeconds(waitTime);
+        displayTimer.gameObject.SetActive(false);
         spawner.gameObject.SetActive(true);
-
     }
 }
